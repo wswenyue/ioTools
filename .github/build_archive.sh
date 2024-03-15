19c72683517058bf4328fb4d3655cb2f4e702d2a
wswenyue <wswenyue@163.com>
2024-03-15 20:11:43 +0800

    shell build

---
+++ .github/build_archive.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Author: wswenyue.
+#Date & Time: 2024-03-15 19:55:32
+#Description: a bash script
+binary_name="${APP_NAME}"
+version="${APP_VERSION}"
+# shellcheck disable=SC2034
+dir_name="${binary_name}-${version}-${{MATRIX_TARGET}}"
+mkdir "${{ dir_name }}"
+# shellcheck disable=SC2193
+if [ "${{ MATRIX_OS }}" = "windows-latest" ]; then
+  mv "target/${MATRIX_TARGET}/release/${{ binary_name }}.exe" "${{ dir_name }}"
+else
+  mv "target/${{ MATRIX_TARGET }}/release/${{ binary_name }}" "${{ dir_name }}"
+fi
+
+if [ "${{ MATRIX_OS }}" = "windows-latest" ]; then
+  7z a "${{ dir_name }}.zip" "${{ dir_name }}"
+  echo "ASSET=${{ dir_name }}.zip" >> $GITHUB_ENV
+else
+  tar -czf "${{ dir_name }}.tar.gz" "${{ dir_name }}"
+  echo "ASSET=${{ dir_name }}.tar.gz" >> $GITHUB_ENV
+fi
