eaca4a9c69ab82e55f63b59114996c4772e95e21
wswenyue <wswenyue@163.com>
2024-03-15 20:18:46 +0800

    shell build

--- .github/build_archive.sh
+++ .github/build_archive.sh
@@ -8,17 +8,18 @@ version="${APP_VERSION}"
 dir_name="${binary_name}-${version}-${MATRIX_TARGET}"
 mkdir "${dir_name}"
 # shellcheck disable=SC2193
-if [ "${{ MATRIX_OS }}" = "windows-latest" ]; then
+if [ "${MATRIX_OS}" = "windows-latest" ]; then
   mv "target/${MATRIX_TARGET}/release/${binary_name}.exe" "${dir_name}"
 else
   mv "target/${MATRIX_TARGET}/release/${binary_name}" "${dir_name}"
 fi
 
 # shellcheck disable=SC2193
-if [ "${{ MATRIX_OS }}" = "windows-latest" ]; then
+if [ "${MATRIX_OS}" = "windows-latest" ]; then
   7z a "${dir_name}.zip" "${dir_name}"
   echo "ASSET=${dir_name}.zip" >> $GITHUB_ENV
 else
   tar -czf "${dir_name}.tar.gz" "${dir_name}"
   echo "ASSET=${dir_name}.tar.gz" >> $GITHUB_ENV
 fi
+echo "succeed."
