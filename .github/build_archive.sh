6a5791d9ae1687b639b29e1c8ecc5b09a3746157
wswenyue <wswenyue@163.com>
2024-03-15 20:16:20 +0800

    shell build

--- .github/build_archive.sh
+++ .github/build_archive.sh
@@ -5,19 +5,20 @@
 binary_name="${APP_NAME}"
 version="${APP_VERSION}"
 # shellcheck disable=SC2034
-dir_name="${binary_name}-${version}-${{MATRIX_TARGET}}"
-mkdir "${{ dir_name }}"
+dir_name="${binary_name}-${version}-${MATRIX_TARGET}"
+mkdir "${dir_name}"
 # shellcheck disable=SC2193
 if [ "${{ MATRIX_OS }}" = "windows-latest" ]; then
-  mv "target/${MATRIX_TARGET}/release/${{ binary_name }}.exe" "${{ dir_name }}"
+  mv "target/${MATRIX_TARGET}/release/${binary_name}.exe" "${dir_name}"
 else
-  mv "target/${{ MATRIX_TARGET }}/release/${{ binary_name }}" "${{ dir_name }}"
+  mv "target/${MATRIX_TARGET}/release/${binary_name}" "${dir_name}"
 fi
 
+# shellcheck disable=SC2193
 if [ "${{ MATRIX_OS }}" = "windows-latest" ]; then
-  7z a "${{ dir_name }}.zip" "${{ dir_name }}"
-  echo "ASSET=${{ dir_name }}.zip" >> $GITHUB_ENV
+  7z a "${dir_name}.zip" "${dir_name}"
+  echo "ASSET=${dir_name}.zip" >> $GITHUB_ENV
 else
-  tar -czf "${{ dir_name }}.tar.gz" "${{ dir_name }}"
-  echo "ASSET=${{ dir_name }}.tar.gz" >> $GITHUB_ENV
+  tar -czf "${dir_name}.tar.gz" "${dir_name}"
+  echo "ASSET=${dir_name}.tar.gz" >> $GITHUB_ENV
 fi
