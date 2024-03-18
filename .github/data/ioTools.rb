8aa36a139a09bff97c955f7720598cb68db40b7d
wswenyue <wswenyue@163.com>
2024-03-18 17:34:40 +0800

    MOD:rename

--- .github/data/ioTools.rb
+++ .github/data/ioTools.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class IoTools < Formula
+class Iotools < Formula
   desc "ioTools using rust build."
   homepage "https://github.com/wswenyue/ioTools"
   url "#_url_#"
@@ -9,7 +9,7 @@ class IoTools < Formula
   version '#_version_#'
 
   def install
-    bin.install "ioTools"
+    bin.install "iotools"
   end
 
   test do
