361261010bb2af7eb30cef0885a949e3c093eb73
wswenyue <wswenyue@163.com>
2024-03-19 14:28:18 +0800

    MOD:优化添加man page生成

--- build.rs
+++ build.rs
@@ -1,13 +1,23 @@
 use clap::CommandFactory;
+
 #[path="src/cli.rs"]
 pub mod cli;
 fn main() -> std::io::Result<()> {
-    let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
+    let mut target_dir: ::std::path::PathBuf =
+        ::std::env::var_os("CARGO_MANIFEST_DIR")
+            .unwrap()
+            .into()
+        ;
+    target_dir.push("target");
+    target_dir.push(::std::env::var_os("PROFILE").unwrap());
+    let target_dir = &*target_dir;
+    println!("cargo:warning={:?}", target_dir);
+    let out_dir = std::path::PathBuf::from(target_dir);
     let cmd = cli::Cli::command();
     let man = clap_mangen::Man::new(cmd);
     let mut buffer: Vec<u8> = Default::default();
     man.render(&mut buffer)?;
     std::fs::write(out_dir.join("iotools.1"), buffer)?;
-    println!("build ok...");
+    println!("cargo:rerun-if-changed=build.rs");
     Ok(())
 }
